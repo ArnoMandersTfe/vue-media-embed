1b80f8dad54ee51040e25a0589ced009132800b6
Arjen Weeber <a.weeber@tfe.nl>
2020-09-09 15:07:48 +0200

    Added extrta check to fix issues with getDuration being called when player is not yet loaded

--- src/player/youtube.js
+++ src/player/youtube.js
@@ -1,71 +1,78 @@
-import Player from './player'
-import PlayerState from './../component/player/player-state'
+import Player from "./player";
+import PlayerState from "./../component/player/player-state";
 
 export default class YouTube extends Player {
-  pauseAfterSeek = false
-  constructor (el, id, props, onStateChange) {
-    super(el, id, props, null, onStateChange)
-    this.onStateChange = this.onStateChange.bind(this)
-    super.load('//www.youtube.com/iframe_api', 'YT.Player')
-  }
-  onLoaded (Player) {
-    this.props.events.onStateChange = this.onStateChange
-    this.player = new Player(this.el, this.props)
-  }
-  getCurrentTime () {
-    return this.player && typeof this.player.getCurrentTime === 'function' ? this.player.getCurrentTime() : 0
-  }
-  getDuration () {
-    if (!this.totalTime && typeof this.player.getDuration === 'function') this.totalTime = this.player.getDuration()
-    return super.getDuration()
-  }
-  seekTo (time, pauseAfterSeek) {
-    this.pauseAfterSeek = pauseAfterSeek
-    this.player.seekTo(time)
-  }
-  play () {
-    this.player.playVideo()
-  }
-  pause () {
-    this.player.pauseVideo()
-  }
-  onStateChange ({ data }) {
-    let state = ''
-    switch (data) {
-      case -3:
-        state = PlayerState.LOADING
-        break
-      case -2:
-        state = PlayerState.LOADED
-        break
-      case -1:
-        state = PlayerState.UNSTARTED
-        break
-      case 0:
-        state = PlayerState.ENDED
-        break
-      case 1:
-        if (this.pauseAfterSeek) {
-          this.pauseAfterSeek = false
-          this.pause()
-          return
-        }
-        state = PlayerState.PLAYING
+	pauseAfterSeek = false;
+	constructor(el, id, props, onStateChange) {
+		super(el, id, props, null, onStateChange);
+		this.onStateChange = this.onStateChange.bind(this);
+		super.load("//www.youtube.com/iframe_api", "YT.Player");
+	}
+	onLoaded(Player) {
+		this.props.events.onStateChange = this.onStateChange;
+		this.player = new Player(this.el, this.props);
+	}
+	getCurrentTime() {
+		return this.player && typeof this.player.getCurrentTime === "function"
+			? this.player.getCurrentTime()
+			: 0;
+	}
+	getDuration() {
+		if (
+			!this.totalTime &&
+			this.player &&
+			typeof this.player.getDuration === "function"
+		)
+			this.totalTime = this.player.getDuration();
+		return super.getDuration();
+	}
+	seekTo(time, pauseAfterSeek) {
+		this.pauseAfterSeek = pauseAfterSeek;
+		this.player.seekTo(time);
+	}
+	play() {
+		this.player.playVideo();
+	}
+	pause() {
+		this.player.pauseVideo();
+	}
+	onStateChange({ data }) {
+		let state = "";
+		switch (data) {
+			case -3:
+				state = PlayerState.LOADING;
+				break;
+			case -2:
+				state = PlayerState.LOADED;
+				break;
+			case -1:
+				state = PlayerState.UNSTARTED;
+				break;
+			case 0:
+				state = PlayerState.ENDED;
+				break;
+			case 1:
+				if (this.pauseAfterSeek) {
+					this.pauseAfterSeek = false;
+					this.pause();
+					return;
+				}
+				state = PlayerState.PLAYING;
 
-        break
-      case 2:
-        state = PlayerState.PAUSED
-        break
-      case 3:
-        state = PlayerState.BUFFERING
-        break
-      case 5:
-        state = PlayerState.CUED
-        break
-      default:
-        console.warn('unhandled state change', data)
-        break
-    }
-    this.setChanged(state)
-  }
+				break;
+			case 2:
+				state = PlayerState.PAUSED;
+				break;
+			case 3:
+				state = PlayerState.BUFFERING;
+				break;
+			case 5:
+				state = PlayerState.CUED;
+				break;
+			default:
+				console.warn("unhandled state change", data);
+				break;
+		}
+		this.setChanged(state);
+	}
 }
